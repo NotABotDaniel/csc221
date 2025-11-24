776a88345dd0ae33516813ee93f8720360b578e9
NotABotDaniel <dcgottshall@icloud.com>
2025-11-24 16:59:16 -0500

    initialize graph: :

---
+++ 4fun/graph.py
@@ -0,0 +1,14 @@
+"""
+  >>> graph = Graph([(2,1),(1,0)])
+  hmmm... not sure yet what I want to do.
+
+"""
+
+class Graph:
+  def __init__(self, coefs = []):
+    pass
+  
+class Point:
+  def __init(self):
+    pass
+
