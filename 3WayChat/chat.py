13eed66a4fe95f2a02aa2105ee78db46bc4c849d
NotABotDaniel <dcgottshall@icloud.com>
2026-01-07 18:26:36 -0500

    now can theoreticaly send to multiple people. also reorganized

--- 3WayChat/chat.py
+++ 3WayChat/chat.py
@@ -3,10 +3,28 @@ import threading
 import curses
 import sqlite3 
 
-target_ip = input("Target IP: ")
-my_port = int(input("My Port: "))
-target_port = int(input("Target Port: "))
-my_name = input("What is your name?")
+addresses = []
+i = 0
+message = ""
+data = ""
+
+class address:
+  def __init__(self, ip, port):
+    self.ip = ip
+    self.port = port
+
+my_port = int(input("Enter your Port: "))
+my_name = input("Enter your name: ")
+
+while True:
+  newIP = input("Enter target IP: ")
+  newPort = input("Enter target Port: ")
+  addresses[i] = address(newIP, newPort)
+  
+  if input("(y/n) Add another address? ") == "y":
+    i += 1
+  else:
+    break
 
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 s.bind(("0.0.0.0", my_port))
@@ -21,30 +39,43 @@ def listen(window):
       window.refresh() 
     except:
       break
+    
+def startListenThread(window):
+  t = threading.Thread(target=listen, args=(window,))
+  t.daemon = True
+  t.start()
 
+def promptForMessage(window):
+  window.clear()
+  window.addstr(0, 0, "You: ")
+  window.refresh()
+  
+  message = window.getstr().decode("utf-8")
+
+def send(window):
+  data = my_name + ":" + message
+    
+  for target in addresses:
+    s.sendto(data.encode("utf-8"), (target.ip, target.port))
+
+  window.addstr("Me: " + message + "\n")
+  window.refresh()
+
+def startMessageing(input_win, chat_win):
+  while True:
+    promptForMessage(input_win)
+    if message == "exit":
+      break
+    
+    send(chat_win)
+  
 def main(stdscr):
   curses.echo() 
   chat_win = curses.newwin(20, 50, 0, 0)
   chat_win.scrollok(True)
   input_win = curses.newwin(5, 50, 21, 0)
   
-  t = threading.Thread(target=listen, args=(chat_win,))
-  t.daemon = True
-  t.start()
-  
-  while True:
-    input_win.clear()
-    input_win.addstr(0, 0, "You: ")
-    input_win.refresh()
-    
-    message = input_win.getstr().decode("utf-8")
-    data = my_name + ":" + message
-    
-    if message == "exit":
-      break
-    
-    s.sendto(data.encode("utf-8"), (target_ip, target_port))
-    chat_win.addstr("Me: " + message + "\n")
-    chat_win.refresh()
+  startListenThread(chat_win)
+  startMessageing(input_win, chat_win)
 
 curses.wrapper(main)
